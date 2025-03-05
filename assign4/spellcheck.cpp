e88250787205453b78b930ba822dfcc26d219c3a
HT4w5 <1683707746@qq.com>
2025-03-05 23:59:24 +0800

    modified:   assign4/spellcheck.cpp

--- assign4/spellcheck.cpp
+++ assign4/spellcheck.cpp
@@ -1,4 +1,4 @@
-//#include "spellcheck.h"
+#include "spellcheck.h"
 
 #include <algorithm>
 #include <iostream>
@@ -10,20 +10,17 @@
 template <typename Iterator, typename UnaryPred>
 std::vector<Iterator> find_all(Iterator begin, Iterator end, UnaryPred pred);
 
-template <typename Iterator>
 Corpus tokenize(const std::string &source)
 {
-  std::vector<Iterator> spaceIts = find_all(source.begin(), source.end(), isspace); // Get all whitespace iterators.
-  Corpus tokens;                                                                    // Store tokens.
-  // Token inserter.
-  std::insert_iterator<Corpus> inserter(tokens, tokens.end());
+  auto spaceIts = find_all(source.begin(), source.end(), isspace); // Get all whitespace iterators.
+  Corpus tokens;                                                   // Store tokens.
   // Transform tokenize lambda.
   auto lambdaTokenize = [&source](std::string::const_iterator it1, std::string::const_iterator it2) -> Token
   {
     return Token(source, it1, it2);
   };
   // Transform to find all tokens.
-  std::transform(spaceIts.begin(), spaceIts.end() - 1, spaceIts.begin() + 1, inserter, lambdaTokenize);
+  std::transform(spaceIts.begin(), spaceIts.end() - 1, spaceIts.begin() + 1, std::inserter(tokens, tokens.end()), lambdaTokenize);
   // Remove empty tokens.
   // Token check lambda.
   auto lambdaCheckEmpty = [](Token tok)
